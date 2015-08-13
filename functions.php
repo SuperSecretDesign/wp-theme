4e0808cc4cc2180acc694daf43e216ab22ff66db
Yana Tzvetkova <yanatzvetkova@gmail.com>
2015-08-13 15:03:33 -0400

    Got the costom logo loading in the footer

--- functions.php
+++ functions.php
@@ -84,6 +84,27 @@ function wmpudev_enqueue_icon_stylesheet() {
 
 add_action( 'wp_enqueue_scripts', 'wmpudev_enqueue_icon_stylesheet' );
 
+// Setting up a logo uploader
+
+function themeslug_theme_customizer( $wp_customize ) {
+    
+	$wp_customize->add_section( 'themeslug_logo_section' , array(
+	    'title'       => __( 'Logo', 'themeslug' ),
+	    'priority'    => 30,
+	    'description' => 'Upload a logo to replace the default site name and description in the header',
+	) );
+
+	$wp_customize->add_setting( 'themeslug_logo' );
+
+	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'themeslug_logo', array(
+	    'label'    => __( 'Logo', 'themeslug' ),
+	    'section'  => 'themeslug_logo_section',
+	    'settings' => 'themeslug_logo',
+	) ) );
+
+}
+add_action( 'customize_register', 'themeslug_theme_customizer' );
+
 
 /* Custom Title Tags */
 
